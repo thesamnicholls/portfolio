3afa30c335d9477d1eeb393bf0170d06db06bc32
Samuel Nicholls <sam.nicholls@drpgroup.com>
2019-08-09 16:32:02 +0100

    adding in image

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,9 @@ import React, { Component } from "react"
 class About extends Component {
   render() {
     return (
-        <div>
-          <h1>About Component</h1>
+        <div className='about'>
+          <h1>About</h1>
+          <img src={require('../avatar.jpg')} alt='example alt'/>
           <p>Dolorum porta tellus porttitor, mi porro ipsum numquam. Iure necessitatibus, omnis rem, taciti, facilisi dignissim, viverra! Ratione pulvinar! Dui maiores porta? Diam torquent repellat. Nesciunt optio? Primis. Potenti, conubia volutpat, quibusdam iusto integer architecto, exercitationem hic officia ut. Molestiae molestiae? Porttitor. Suspendisse, eaque quod rutrum egestas voluptate officiis adipiscing metus.</p>
         </div>
     )
