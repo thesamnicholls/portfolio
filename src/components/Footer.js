67b21c7e4f48a49b3e8efa84b5934c15674d58d1
Samuel Nicholls <sam.nicholls@drpgroup.com>
2019-08-09 16:10:00 +0100

    adding in left align to everything

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
         <footer className='footer'>
-          <p>Click <a href='https://github.com/'>here</a> to view this webiste on GitHub</p>
+          <p className='footer__text'>Click <a href='https://github.com/'>here</a> to view this webiste on GitHub</p>
         </footer>
     )
   }
