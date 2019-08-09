5fde70a08586c4f5f31e20c439e97108e94b0bcf
Samuel Nicholls <sam.nicholls@drpgroup.com>
2019-08-09 12:50:26 +0100

    refactoring code and adding in new favicon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,11 @@ import React, { Component } from "react"
 class Navigation extends Component {
   render() {
     return (
-        <div className='navigation-container'>
+        <div className='outer-container'>
             <div className='navigation'>
-              <a className="site-logo transition-link" href="/">
-                <span className="logo-text">SN</span>
-              </a>
+              <div className="navigation__logo">
+                <span className="navigation__name _bold">SN</span>
+              </div>
             </div>
         </div>
     )
