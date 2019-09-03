b51bbe468fcecc27fb47853756cec43dbd14d69c
Samuel Nicholls <sam.nicholls@drpgroup.com>
2019-09-03 21:01:48 +0100

    Fixing mess with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.scss';
-import App from './components/App';
+import App from './js/components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
