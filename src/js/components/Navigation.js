41d94df0e3d68867ee85f88bf8b31f99582c8936
Samuel Nicholls <sam.nicholls@drpgroup.com>
2019-12-23 13:39:11 +0000

    Adding in burger menu

--- src/js/components/Navigation.js
+++ src/js/components/Navigation.js
@@ -5,9 +5,23 @@ class Navigation extends Component {
     return (
       <div className='l-container'>
         <div className='o-navigation'>
-          <div className="o-navigation__logo">
-            <span className="o-navigation__name _bold">SN</span>
+          <div className='o-navigation__logo'>
+            <span className='o-navigation__name _bold'>SN</span>
           </div>
+          <div className='o-navigation__burger'>
+            <div className='o-navigation__menu'></div>
+          </div>
+          <ul className='o-navigation__list'>
+            <li className='o-navigation__item'>
+              About
+            </li>
+            <li className='o-navigation__item'>
+              Work
+            </li>
+            <li className='o-navigation__item'>
+              Contact
+            </li>
+          </ul>
         </div>
       </div>
     )
@@ -15,3 +29,4 @@ class Navigation extends Component {
 }
 
 export default Navigation;
+
