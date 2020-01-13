1a10eaa9869fe00600358f625f7654a59d79d679
Samuel Nicholls <sam.nicholls@drpgroup.com>
2020-01-13 19:30:18 +0000

    Testing out vh

--- src/js/components/Intro.js
+++ src/js/components/Intro.js
@@ -6,14 +6,26 @@ class Intro extends Component {
     return (
       <div className='l-container'>
         <div className='c-intro'>
-          {/* <h1 className='c-intro__text'>
-            Hello, I'm Samuel Nicholls. 
-            I'm currently working as a Front End Developer at DRPG and i'm passionate about web development.
-          </h1> */}
-          <ReactTypingEffect
-            text="I'm Samuel Nicholls. I'm a Front End Developer"
+          <h1 className='lfm__typing_effect'>
+            Samuel Nicholls, Front End Developer
+          </h1>
+          {/* <ReactTypingEffect
+            text="Samuel Nicholls, Front End Developer"
             speed="100"
-          />
+          /> */}
+        </div>
+        <div className='c-contact'>
+          <ul className='c-contact__list'>
+            <li className='c-contact__item'>
+              <a href="https://google.co.uk" className='c-contact__link'>Twitter</a>
+            </li>
+            <li className='c-contact__item'>
+              <a href="https://google.co.uk" className='c-contact__link'>Codepen</a>
+            </li>
+            <li className='c-contact__item'>
+              <a href="https://google.co.uk" className='c-contact__link'>GitHub</a>
+            </li>
+          </ul>
         </div>
       </div>
     )
